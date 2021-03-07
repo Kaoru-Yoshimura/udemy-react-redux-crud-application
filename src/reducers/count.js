ff50b522dc80e81a7a175b3b13ae7b086eb5ebef
Kaoru1993 <kaoruyoshimura_1027@yahoo.co.jp>
2021-03-07 16:52:26 +0900

    implement store

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,6 +1,6 @@
 import { INCREMENT, DECREMENT } from '../actions'
 
-const initialState = { value: 0 }
+const initialState = { value: 0 };
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, action) => {
     switch (action.type) {
